4cace0e994ce8ba4dc6633e5f2ecf16d6a524487
MohammadFakhrizaMaftukhin <fakhriza18b@gmail.com>
2026-05-31 15:31:43 +0700

    Inisialisasi Proyek, Metrics, dan Selenium E2E

---
+++ tests/SystemTest.php
@@ -0,0 +1,53 @@
+<?php
+use PHPUnit\Framework\TestCase;
+use Facebook\WebDriver\Remote\RemoteWebDriver;
+use Facebook\WebDriver\Remote\DesiredCapabilities;
+use Facebook\WebDriver\Chrome\ChromeOptions;
+use Facebook\WebDriver\WebDriverBy;
+use Facebook\WebDriver\WebDriverExpectedCondition; // 1. Wajib tambahkan ini
+
+class SystemTest extends TestCase{
+    private $driver;
+    private $baseUrl = 'http://localhost:8000';
+
+    protected function setUp(): void{
+        $host = 'http://localhost:9515';
+        $chromeOptions = new ChromeOptions();
+        $chromeOptions->addArguments(['--headless', '--disable-gpu', '--no-sandbox']);
+
+        $capabilities = DesiredCapabilities::chrome();
+        $capabilities->setCapability(ChromeOptions::CAPABILITY, $chromeOptions);
+
+        $this->driver = RemoteWebDriver::create($host, $capabilities);
+    }
+
+    public function testHomepageAndSearchFeature(){
+        // Kunjungi server lokal
+        $this->driver->get($this->baseUrl);
+
+        // Validasi antarmuka memuat teks judul
+        $bodyText = $this->driver->findElement(WebDriverBy::tagName('body'))->getText();
+        $this->assertStringContainsString('Toko Online', $bodyText);
+
+        // Simulasi pengguna mencari barang
+        $searchBox = $this->driver->findElement(WebDriverBy::name('cari'));
+        $searchBox->sendKeys('Kemeja');
+        $searchBox->submit(); // Meminta browser melakukan refresh
+
+        // 2. PENYELESAIAN MASALAH (EXPLICIT WAIT)
+        // Paksa driver menunggu (maksimal 5 detik) hingga URL berubah
+        $this->driver->wait(5)->until(
+            WebDriverExpectedCondition::urlContains('cari=Kemeja')
+        );
+
+        // 3. Setelah URL berubah, DOM dipastikan sudah baru dan aman untuk dicari ulang
+        $updatedBodyText = $this->driver->findElement(WebDriverBy::tagName('body'))->getText();
+        $this->assertStringContainsString('Kemeja Flanel', $updatedBodyText);
+    }
+
+    protected function tearDown(): void{
+        if ($this->driver) {
+            $this->driver->quit(); // Tutup browser
+        }
+    }
+}
